4925827549f6b3a567823bb3e8607444dc35c9b0
Yusuke Iwama <yusuke.iwama@gmail.com>
2014-02-12 15:36:19 +0900

    Initial Commit

---
+++ USKQueueSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  USKQueueSample
+//
+//  Created by Yusuke Iwama on 2/12/14.
+//  Copyright (c) 2014 University of Tsukuba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
